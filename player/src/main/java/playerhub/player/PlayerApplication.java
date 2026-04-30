eaa8d396040c00c9593adbecf2dd6df062f998f0
joa851 <joa851@inlumine.ual.es>
2026-05-01 00:24:44 +0200

    player initial commit

---
+++ player/src/main/java/playerhub/player/PlayerApplication.java
@@ -0,0 +1,13 @@
+package playerhub.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerApplication.class, args);
+	}
+
+}
